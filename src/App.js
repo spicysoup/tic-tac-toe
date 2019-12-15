fdf6c3588a2b67f2bf6dbe793af41ab9ca29ea1d
Zhuang Xu <not-real@example.com>
2019-12-15 19:55:22 +1100

    Refactorted to use React and SVG

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { NavLink, Route, Switch } from 'react-router-dom';
 import Dummy from 'components/Dummy';
+import Board from 'components/Board';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,11 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
+          Welcome to Tic-Tac-Toe
         </p>
         <a
           className="App-link"
@@ -21,10 +19,20 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn the Rules
         </a>
+        <button type="button">
+          <NavLink to="/board">Go to the Game</NavLink>
+        </button>
       </header>
-      <Dummy />
+      <Switch>
+        <Route exact path="/">
+          <Dummy />
+        </Route>
+        <Route path="/board">
+          <Board dimension={10} />
+        </Route>
+      </Switch>
     </div>
   );
 }
